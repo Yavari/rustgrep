105c6a19c7c04446b90f205470a5ef84d5905c66
Payam Yavari <payam@yavtech.se>
2022-08-14 09:23:31 +0200

    Use threads for search

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,9 @@
 use std::{
     error,
-    fs::{File},
-    io::{BufRead, BufReader}, path::PathBuf,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+    thread,
 };
 
 pub struct SearchResult {
@@ -15,32 +17,48 @@ pub fn search(
     files: Vec<PathBuf>,
     query: String,
 ) -> Result<Vec<SearchResult>, Box<dyn error::Error>> {
-    let mut result = Vec::new();
-    for path in files {
-        let file = File::open(&path)?;
-        let reader = BufReader::new(file);
-        reader
-            .lines()
-            .flatten()
-            .enumerate()
-            .for_each(|(line, content)| {
-                if content.contains(&query) {
-                    if let Some(x) = path.to_str() {
-                        let start_index = get_start_index(&content, &query);
-                        if let Some(si) = start_index {
-                            result.push(SearchResult {
-                                path: x.into(),
-                                content: content.trim().to_string(),
-                                line: line + 1,
-                                column: si + 1,
-                            });
+    let threads = files.into_iter().map(|path| {
+        let a = query.to_string();
+        thread::spawn(move || {
+            let mut result = Vec::new();
+            let file = File::open(&path);
+            if let Ok(file) = file {
+                let reader = BufReader::new(file);
+                reader
+                    .lines()
+                    .flatten()
+                    .enumerate()
+                    .for_each(|(line, content)| {
+                        if content.contains(&a) {
+                            if let Some(x) = path.to_str() {
+                                let start_index = get_start_index(&content, &a);
+                                println!("{}", x);
+                                if let Some(si) = start_index {
+                                    result.push(SearchResult {
+                                        path: x.into(),
+                                        content: content.trim().to_string(),
+                                        line: line + 1,
+                                        column: si + 1,
+                                    });
+                                }
+                            }
                         }
-                    }
-                }
-            });
+                    });
+            }
+
+            result
+        })
+    });
+
+    let mut search_results = Vec::new();
+    for thread in threads {
+        let result = thread.join().unwrap();
+        for item in result {
+            search_results.push(item);
+        }
     }
 
-    Ok(result)
+    Ok(search_results)
 }
 
 fn get_start_index(content: &str, query: &str) -> Option<usize> {
@@ -50,7 +68,7 @@ fn get_start_index(content: &str, query: &str) -> Option<usize> {
 
     for (index, char) in content.chars().enumerate() {
         if char == query[temp] {
-            if temp < length -1 {
+            if temp < length - 1 {
                 temp += 1;
             } else {
                 return Some(index + 1 - length);
